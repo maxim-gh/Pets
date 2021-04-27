91ed90b4ce162e6408fb3c50da76bc6bf61593b2
maxim-gh <max.verzilov32@gmail.com>
2021-04-27 19:27:15 +0500

    Date

--- Animal/Program.cs
+++ Animal/Program.cs
@@ -9,10 +9,10 @@ namespace Animal
             var calculator= new Calculator();
             var visitor = new Visitor[]
             {
-                new Visitor("Василий", "Васильев", "+76343346346", new Pet ("Шарик", "Овчарка", PetType.Dog, 4)),
-                new Visitor("Владимир", "Владимиров", "+73457983455", new Pet ("Пушок", "Манчкин", PetType.Cat, 2)),
-                new Visitor("Петр", "Петров", "+79238745736", new Pet ("Алиса", "Британская короткошорстная", PetType.Cat, 3)),
-                new Visitor("Николай", "Николаев", "+70983453784", new Pet ("Бобик", "Такса", PetType.Dog, 1))
+                new Visitor("Василий", "Васильев", "+76343346346", new DateTime(2021, 2, 23), new Pet ("Шарик", "Овчарка", PetType.Dog, 4)),
+                new Visitor("Владимир", "Владимиров", "+73457983455", new DateTime(2020, 12, 4), new Pet ("Пушок", "Манчкин", PetType.Cat, 2)),
+                new Visitor("Петр", "Петров", "+79238745736", new DateTime(2021, 4, 1), new Pet ("Алиса", "Британская короткошорстная", PetType.Cat, 3)),
+                new Visitor("Николай", "Николаев", "+70983453784", new DateTime(2020, 8, 12), new Pet ("Бобик", "Такса", PetType.Dog, 1))
             };
 
             var i = 1;
@@ -20,7 +20,7 @@ namespace Animal
             {
                 Console.Write($"{i}) ");
                 i++;
-                Console.Write($"Клиент {element.FirstName} { element.Surname} привел питомца ");
+                Console.Write($"Клиент {element.FirstName} { element.Surname} привел питомца {element.DateTime} ");
                 Console.WriteLine($"по кличке {element.Pet.NickName}, порода {element.Pet.Breed}, вид питомца {element.Pet.PetType}.");
                 Console.WriteLine($"Cтоимость услуги {calculator.PriceCalculator(element.Pet.PetType,element.Pet.DiffcultyFactor)}.");
                 
--- Animal/Visitor.cs
+++ Animal/Visitor.cs
@@ -5,13 +5,15 @@ public class Visitor
 	public string FirstName { get; }
 	public string Surname { get; }
 	public string PhoneNumber { get; }
+    public DateTime DateTime { get; }
     public Pet Pet { get; }
 
-    public Visitor(string firstName, string surname, string phoneNumber, Pet pet)
+    public Visitor(string firstName, string surname, string phoneNumber, DateTime dateTime, Pet pet)
     {
         FirstName = firstName;
         Surname = surname;
         PhoneNumber = phoneNumber;
+        DateTime = dateTime;
         Pet = pet;
     }
 }
